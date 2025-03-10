4266da3105d3e3d313bce69bb028f5800ebf3144
unknown <YY235@ccs.local>
2025-03-10 05:44:36 -0700

    rename repo and change name in respond for task

---
+++ question3.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! John Clarence Palma' });
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+});
